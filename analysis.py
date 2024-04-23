4214d7ec86d89aca7d236c9ed6870ae4a7d62d49
Tsveta Kodzhabasheva <tsveta.kodzhabasheva@gmail.com>
2024-04-23 23:09:28 +0200

    putting analysis ideas

--- analysis.py
+++ analysis.py
@@ -10,19 +10,35 @@ import pprint
 from visualize import parse_coordinates, parse_imports
 
 def analyse(nodes, edges):
-    G = nx.DiGraph()
-    G.add_nodes_from(nodes.keys())
+    G_directed = nx.DiGraph()
+    G_directed.add_nodes_from(nodes.keys())
+
+    G_undirected = nx.Graph()
+    G_undirected.add_nodes_from(nodes.keys())
+
+
     ebunch = list(map(lambda e: (e.source, e.target, {"quantity": e.quantity, "percent_imports": e.percent_imports, "percent_of_all_for_site": e.percent_of_all_for_site}), edges))
-    G.add_edges_from(ebunch)
+    G_directed.add_edges_from(ebunch)
+    G_undirected.add_edges_from(ebunch)
 
     positions = nodes
 
-    print(nx.average_neighbor_degree(G, source="out", target="in", weight="percent_imports"))
+    # print('Average neighbor degree',nx.average_neighbor_degree(G_directed, source="in", weight="percent_of_all_for_site")) #useufl
+    # print('Indegree centrality', nx.in_degree_centrality(G_directed)) #useful
+    # print(nx.sigma(G_undirected)) #not useful
+    # print(nx.omega(G_undirected)) #not useful
+    # print(nx.is_weakly_connected(G_directed))
+    # print('Karz centrality',nx.katz_centrality(G_directed, weight='percent_of_all_for_site"')) # useful
+    # print(nx.trophic_levels(G_directed, weight='percent_of_all_for_site')) #not useful - visible without this
+    # print('Clustering',nx.clustering(G_directed)) #not useful
+    # print('Page rank', nx.pagerank(G_directed)) #useful
+    # reciprocity - not useful for our network but perhaps in another one
+    # comparing two or more network's similarity 
 
 
 
 
- 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
                     prog='ProgramName',
