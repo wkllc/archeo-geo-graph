e70c604ebb5086de13a9fcd5a6a3c6261eb2b993
Dimitar Dimitrov <dimitrov.dna@gmail.com>
2025-07-21 21:17:58 +0200

    restructured project file system, ensured data files are correctly referenced

---
+++ scripts/analysis.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+from collections import namedtuple
+from collections import Counter
+from networkx.algorithms import bipartite
+
+
+import argparse
+import pandas as pd
+import pprint
+
+from scripts.visualize import parse_coordinates, parse_imports
+
+
+def analyse(nodes, edges):
+    G = nx.DiGraph()
+    G.add_nodes_from(nodes)
+
+    ebunch = list(map(lambda e: (e.source, e.target, {
+        "source": e.source,
+        "target": e.target,
+        "quantity": e.quantity,
+        "percent_imports": e.percent_imports,
+        "percent_of_all_for_site": e.percent_of_all_for_site}), edges))
+
+    G.add_edges_from(ebunch)
+
+    positions = nx.spring_layout(G, k=3, iterations=200)
+
+    #nx.draw_networkx(G, positions)
+    #plt.show()
+
+    print('Average neighbor indegree',nx.average_neighbor_degree(G, source="in+out"))
+    print('Average neighbor indegree',nx.average_neighbor_degree(G, source="in"))
+
+    #print('Indegree centrality', nx.in_degree_centrality(G))
+    #print('Outdegree centrality', nx.out_degree_centrality(G))
+    #print('Katz centrality',nx.katz_centrality(G))
+    #print('Page rank', nx.pagerank(G))
+    #print('HITS', nx.hits(G))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+                    prog='ProgramName',
+                    description='What the program does',
+                    epilog='Text at the bottom of help')
+
+    parser.add_argument('-coordinates', type=str, default="data/Coordinates.csv")
+    parser.add_argument('-imports', type=str, default="data/imports_percent_combined_version4.csv")
+    parser.add_argument('-century', type=int, default='18')
+
+    args = parser.parse_args()
+    node_coordinates = parse_coordinates(args.coordinates)
+    nodes, edges = parse_imports(node_coordinates, args.imports, args.century)
+    analyse(nodes, edges)
+    
---
+++ scripts/compare_networks.py
@@ -0,0 +1,31 @@
+import pickle
+import matplotlib.pyplot as plt
+import networkx as nx
+from collections import namedtuple
+
+import argparse
+import pandas as pd
+
+with open("data/15_combined.pickle", "rb") as f:
+    G_15 = pickle.load(f)
+
+with open("data/16_combined.pickle", "rb") as f:
+    G_16 = pickle.load(f)
+
+with open("data/17_combined.pickle", "rb") as f:
+    G_17 = pickle.load(f)
+
+with open("data/18_combined.pickle", "rb") as f:
+    G_18 = pickle.load(f)
+
+with open("data/19_combined.pickle", "rb") as f:
+    G_19 = pickle.load(f)
+
+with open("data/20_combined.pickle", "rb") as f:
+    G_20 = pickle.load(f)
+
+print('Graph Edit Distance between 15 and 16 centuries',nx.graph_edit_distance(G_15,G_16))
+#print('Graph Edit Distance between 16 and 17 centuries',nx.graph_edit_distance(G_16,G_17))
+#print('Graph Edit Dissance between 17 and 18 centuries',nx.graph_edit_distance(G_17,G_18))
+#print('Graph Edit Distance between 18 and 19 centuries',nx.graph_edit_distance(G_18,G_19))
+#print('Graph Edit Distance between 19 and 20 centuries',nx.graph_edit_distance(G_19,G_20))
