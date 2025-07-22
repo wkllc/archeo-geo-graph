98a25369b6dc1a9a0b47ace75458c6d1856ec90c
Dimitar Dimitrov <dimitrov.dna@gmail.com>
2025-07-22 19:32:37 +0200

    replaced precomputed pickles with a script for comparing networks

--- scripts/compare_networks.py
+++ scripts/compare_networks.py
@@ -6,26 +6,54 @@ from collections import namedtuple
 import argparse
 import pandas as pd
 
-with open("data/15_combined.pickle", "rb") as f:
-    G_15 = pickle.load(f)
+from scripts.data_loader import load_network_data
 
-with open("data/16_combined.pickle", "rb") as f:
-    G_16 = pickle.load(f)
+def network_comparison(imports_path, coordinates_path, century_1, century_2):
+    """
+    Compare two network graphs from different centuries.
 
-with open("data/17_combined.pickle", "rb") as f:
-    G_17 = pickle.load(f)
+    Args:
+        fpath: Path to the pickle file containing the network data.
+        century_1: First century to compare.
+        century_2: Second century to compare.
 
-with open("data/18_combined.pickle", "rb") as f:
-    G_18 = pickle.load(f)
+    Returns:
+        None
+    """
 
-with open("data/19_combined.pickle", "rb") as f:
-    G_19 = pickle.load(f)
+    nodes, edges = load_network_data(coordinates_file=coordinates_path, imports_file=imports_path, century=century_1)
+    G_1 = nx.from_pandas_edgelist(edges,
+                                  source='source',
+                                  target='target',
+                                  edge_attr=True,
+                                  create_using=nx.DiGraph())
+    
 
-with open("data/20_combined.pickle", "rb") as f:
-    G_20 = pickle.load(f)
+    nodes, edges = load_network_data(coordinates_file=coordinates_path, imports_file=imports_path, century=century_2)
+    G_2 = nx.from_pandas_edgelist(edges,
+                                  source='source',
+                                  target='target',
+                                  edge_attr=True,
+                                  create_using=nx.DiGraph())
 
-print('Graph Edit Distance between 15 and 16 centuries',nx.graph_edit_distance(G_15,G_16))
-#print('Graph Edit Distance between 16 and 17 centuries',nx.graph_edit_distance(G_16,G_17))
-#print('Graph Edit Dissance between 17 and 18 centuries',nx.graph_edit_distance(G_17,G_18))
-#print('Graph Edit Distance between 18 and 19 centuries',nx.graph_edit_distance(G_18,G_19))
-#print('Graph Edit Distance between 19 and 20 centuries',nx.graph_edit_distance(G_19,G_20))
+
+    print(f"Graph Edit Distance between imports networks of centuries {century_1} and {century_2}: {nx.graph_edit_distance(G_1, G_2)}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+                    prog='comapre_networks',
+                    description='Compare the network graphs of 2 different centuries',
+                    epilog='made by Tsveta and Dimitar')
+
+    parser.add_argument('-coordinates', type=str, default="data/Coordinates.csv")
+    parser.add_argument('-imports', type=str, default="data/imports_percent_combined_version4.csv")
+    parser.add_argument('-century_1', required=False, type=int, default='15')
+    parser.add_argument('-century_2', required=False, type=int, default='16')
+
+    args = parser.parse_args()
+
+    network_comparison(imports_path = args.imports,
+                       coordinates_path = args.coordinates,
+                       century_1 = args.century_1,
+                       century_2 = args.century_2)
